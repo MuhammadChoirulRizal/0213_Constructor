6dd7c5ea14198b0a4d59755f9ebaa2703cac727f
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2025-05-17 14:56:58 +0700

    membuat class angka dan atributnya

--- destructor.cpp
+++ destructor.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
+class angka
+{
+private:
+    int *arr;
+    int panjang;
 
-class angka{
+public:
+    angka(int); // Constructor
+    ~angka();   // Destructor
+    void cetakData();
+    void isiData();
 };
